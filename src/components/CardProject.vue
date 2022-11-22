a08db774cda4a3b4b562d59e0ecb1ff99205e3b4
Dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2022-11-21 20:39:41 -0600

    first commit

---
+++ src/components/CardProject.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card horizontal">
+        <div class="card-stacked">
+            <div class="card-content">
+                <span class="card-title">Proyecto 1</span>
+                <p> Descripción del proyecto</p>
+                <p>
+                    Tecnologías:
+                </p>
+            </div>
+            <div class="card-action">
+                <a href="#" class="amber darken-3 col s5 waves-effect waves-light btn"><i class="material-icons">create</i></a>
+                <a href="#" class="red darken-3 col s5 waves-effect waves-light btn"><i class="material-icons">delete</i></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+a{
+    margin: 0px 2vw;
+}
+</style>
---
+++ src/components/Contenedor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col s12 m7">
+        
+        <card-project/>
+    </div>
+  </div>
+</template>
+
+<script>
+    import CardProject from './CardProject.vue'
+export default {
+  data:()=>({
+    projects:null,
+  }),
+    components:{
+        CardProject,
+    },
+    mounted(){
+      this.getProjects()
+
+    },methods:{
+      async getProjects(){
+        //link del realtime database de cada uno
+        const res = await fetch("https://crud-vue-4b-92ebf-default-rtdb.firebaseio.com/projects/project.json");
+        const data = await res.json();
+        console.log(data);
+      },
+    },
+};
+</script>
+
+<style>
+
+</style>
